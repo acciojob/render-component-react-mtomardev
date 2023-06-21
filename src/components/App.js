530ff0a42788960b6060acc4b300019b6ec7508c
mtomardev <120013813+mtomardev@users.noreply.github.com>
2023-06-21 17:03:51 +0530

    first code

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,13 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <div id="root">
+        <p>I am learning React. My life is getting better.</p>
+
+      </div>
+
+    </div>
   )
 }
 
